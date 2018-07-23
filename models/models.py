efb46d8c13ea0b212766144a42b370e2c0d5ebe4
jeffery chen fan <jeffery9@gmail.com>
2018-07-23 14:34:59 +0800

    migrated to 11.0

--- models/models.py
+++ models/models.py
@@ -9,11 +9,11 @@ class IrMailServer(models.Model):
     email = fields.Char(string=u'Email', required=True, help="Email地址, 例如 postmaster@domain.tld")
 
     @api.model
-    def send_email(self, message, mail_server_id=None, smtp_server=None, smtp_port=None, smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False ):
+    def send_email(self, message, mail_server_id=None, smtp_server=None, smtp_port=None, smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False, smtp_session=None ):
 
         origin_mail_form = message['From']
         if mail_server_id:
             message.replace_header('From', self.sudo().browse(mail_server_id).email)
             message['Sender'] = origin_mail_form
 
-        return super(IrMailServer, self).send_email(message, mail_server_id, smtp_server, smtp_port, smtp_user, smtp_password, smtp_encryption, smtp_debug )
+        return super(IrMailServer, self).send_email(message, mail_server_id, smtp_server, smtp_port, smtp_user, smtp_password, smtp_encryption, smtp_debug, smtp_session)
